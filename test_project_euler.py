a9353f105eecf707f87c8ad3ac915f21a051561a
ledwindra <lukman.edwindra@gmail.com>
2020-07-30 15:50:03 +0700

    solved problem three

--- test_project_euler.py
+++ test_project_euler.py
@@ -10,9 +10,14 @@ class TestProblemOne(unittest.TestCase):
         self.assertEqual(answer, 233168)
 
     def test_problem_two(self):
-        answer = self.project_euler.problem_two()
+        answer = self.project_euler.problem_two(4000000)
 
         self.assertEqual(answer, 4613732)
+
+    def test_problem_three(self):
+        answer = self.project_euler.problem_three(600851475143)
+
+        self.assertEqual(answer, 6857)
 
 if __name__ == "__main__":
     unittest.main()
